dfa8007615850a8553da549475c46d543950f56f
buyi <9440979@qq.com>
2018-11-22 17:22:24 +0800

    A 新增对称加密

---
+++ src/test/java/com/buyi/util/security/AESUtilTest.java
@@ -0,0 +1,26 @@
+package com.buyi.util.security;
+
+import org.testng.annotations.Test;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import static org.testng.Assert.*;
+
+public class AESUtilTest {
+
+    @Test
+    public void testEncode() throws NoSuchPaddingException, UnsupportedEncodingException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        String str = "孙二";
+        String key = "80e36e39f34e678c";
+        String encodeStr = AESUtil.encode(key, str);
+        System.out.println("加密后：" + encodeStr);
+        String decodeStr = AESUtil.decode(key, encodeStr);
+        assertEquals(str, decodeStr);
+    }
+
+}
